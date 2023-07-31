573744f79b92c8c15493a39015d6a1da3894f6bf
N-Hidaya <nurul-h@hotmail.com>
2023-07-31 23:16:57 +0800

    added email nodemailer

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,18 +1,59 @@
 import Link from 'next/link';
-import React from 'react'
+import React, {useState} from 'react'
 import { AiOutlineMail } from "react-icons/ai";
 import { FaGithub, FaInstagram } from "react-icons/fa";
 import { RiContactsFill } from "react-icons/ri";
 import {HiOutlineChevronDoubleUp} from "react-icons/hi"
+import { sendContactForm } from '../lib/api';
+
+
+const initValues = { name: "", email: "", country: "", subject: "", message: ""}
+
+const initState = { values: initValues}
+
 
 const Contact = () => {
+
+    const [state, setState] = useState(initState);
+
+    const { values, isLoading, error } = state;
+
+
+    const handleChange = ({target}) => setState((prev) => ({
+        ...prev,
+        values: {
+            ...prev.values,
+            [target.name]: target.value,
+        }
+    }))
+
+    const onSubmit = async () => {
+        setState((prev) => ({
+            ...prev,
+            isLoading: true,
+        }));
+
+        try {
+            await sendContactForm(values);
+            setState(initState)
+        } catch (error) {
+            setState((prev) => ({
+                ...prev,
+                isLoading: false,
+                error: error.message
+            }));
+        }
+    }
+
   return (
     <div id='contact' className='w-full lg:h-screen'>
       <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
         <p className='text-xl tracking-widest uppercase text-white drop-shadow-lg'>
             Contact
         </p>
+        
         <h2 className='py-4 text-[#402D29]'>Get in Touch</h2>
+        
         <div className='grid lg:grid-cols-5 gap-8'>
             <div className='col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-white rounded-xl p-4'>
                 <div className='lg:p-4 h-full'>
@@ -49,31 +90,34 @@ const Contact = () => {
             </div>
 
             <div className='col-span-3 w-full h-auto shadow-xl shadow-white rounded-xl lg:p-4'>
+            {error && (
+            <p className='text-[#f75c51] text-xl p-4'>{error}</p> 
+            )}
+        
                 <div className='p-4'>
-                    <form>
-                        <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
-                            <div className='flex flex-col'>
-                                <label className='uppercase text-sm py-2'>Name</label>
-                                <input type='text' className='border-2 rounded-lg p-3 flex border-gray-300 '/>
-                            </div>
-                            <div className='flex flex-col'>
-                                <label className='uppercase text-sm py-2'>Phone number</label>
-                                <input type='text' className='border-2 rounded-lg p-3 flex border-gray-300 '/>
-                            </div>
+                    <form action='/api/contactdata' method='post' >
+                           
+                        <div className='flex flex-col py-2'>
+                            <label className='uppercase text-sm py-2 '>Name <span className='text-[#ec504e]'>*</span></label>
+                            <input type='text' id='name' name='name' value={values.name} onChange={handleChange} className='border-2 rounded-lg p-3 flex border-gray-300 required:border-red-500'/>
                         </div>
                         <div className='flex flex-col py-2'>
-                            <label className='uppercase text-sm py-2'>Email</label>
-                            <input type='email' className='border-2 rounded-lg p-3 flex border-gray-300 '/>
+                            <label className='uppercase text-sm py-2'>Email <span className='text-[#ec504e]'>*</span></label>
+                            <input type='email' id='email' name='email' value={values.email} onChange={handleChange} className='border-2 rounded-lg p-3 flex border-gray-300 required:border-red-500'/>
+                        </div>
+                        <div className='flex flex-col py-2'>
+                            <label className='uppercase text-sm py-2'>Country</label>
+                            <input type='text' id='country' name='country' value={values.country} onChange={handleChange} className='border-2 rounded-lg p-3 flex border-gray-300 '/>
                         </div>
                         <div className='flex flex-col py-2'>
                             <label className='uppercase text-sm py-2'>Subject</label>
-                            <input type='text' className='border-2 rounded-lg p-3 flex border-gray-300 '/>
+                            <input type='text' id='subject' name='subject' value={values.subject} onChange={handleChange} className='border-2 rounded-lg p-3 flex border-gray-300 '/>
                         </div>
                         <div className='flex flex-col py-2'>
                             <label className='uppercase text-sm py-2'>Message</label>
-                            <textarea className='border-2 rounded-lg p-3 border-gray-300' rows='10'></textarea>
+                            <textarea id='message' name='message' value={values.message} onChange={handleChange} className='border-2 rounded-lg p-3 border-gray-300' rows='10'></textarea>
                         </div>
-                        <button className='w-full p-4 text-gray-100 mt-4'>Send</button>
+                        <button isloading={isLoading} disabled={!values.name || !values.email} onClick={onSubmit} className='w-full p-4 text-gray-100 mt-4'>Send</button>
                     </form>
                 </div>
             </div>
---
+++ components/Thanksctc.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Thanksctc = () => {
+  return (
+    <div>
+      Thank your for your enquiries! Your message has been sent successfully!
+    </div>
+  )
+}
+
+export default Thanksctc
