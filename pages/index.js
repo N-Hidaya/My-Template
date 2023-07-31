9df6451e4eb7ca9e6d3bf01c7d206c34c7604632
N-Hidaya <nurul-h@hotmail.com>
2023-07-31 17:01:37 +0800

    fixed errors

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@
 import { getSortedPostsData } from '../lib/posts';
 import Head from 'next/head';
 import Main from '../components/Main';
-import About from '../components/about';
+import About from '../components/About';
 import Skills from '../components/Skills';
-import Projects from '../components/projects';
+import Projects from '../components/Projects';
 import Contact from '../components/Contact';
 import Services from '../components/Services';
 
