caa0529adf6ad830f7578ffdebf683eddceae63e
N-Hidaya <nurul-h@hotmail.com>
2023-08-05 17:09:17 +0800

    try agwin

--- pages/Payhome.jsx
+++ pages/Payhome.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import Tos from '../components/Tos'
 
 export const Amount = ({amount, setAmount, value}) => {
   return (
@@ -16,7 +17,7 @@ function Payhome() {
     <div id="payment" className='w-full'>
       <div className='mx-auto px-2 py-20'>
             <p className='text-xl tracking-widest mt-20 uppercase text-center text-white drop-shadow-lg'>Purchasement of Application Contract </p>
-
+            <Tos />
           
             <div className='flex flex-col space-y-4 mx-auto px-4 py-6 items-center rounded-md md:w-10/12'>
                 <h2 className='textBlock-title text-[#402D29]'>Payment</h2>
