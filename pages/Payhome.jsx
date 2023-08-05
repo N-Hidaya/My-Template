7d0609d865f7548e8ec193af111f356299aec289
N-Hidaya <nurul-h@hotmail.com>
2023-08-05 17:03:09 +0800

    tos deletd

--- pages/Payhome.jsx
+++ pages/Payhome.jsx
@@ -1,5 +1,4 @@
 import React, {useState} from 'react'
-import Tos from '../components/tos'
 
 export const Amount = ({amount, setAmount, value}) => {
   return (
@@ -17,7 +16,6 @@ function Payhome() {
     <div id="payment" className='w-full'>
       <div className='mx-auto px-2 py-20'>
             <p className='text-xl tracking-widest mt-20 uppercase text-center text-white drop-shadow-lg'>Purchasement of Application Contract </p>
-            <Tos />
 
           
             <div className='flex flex-col space-y-4 mx-auto px-4 py-6 items-center rounded-md md:w-10/12'>
