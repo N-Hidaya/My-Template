92fe84a8a52b428ceb9da70d9ebf463be9532180
N-Hidaya <nurul-h@hotmail.com>
2023-08-17 16:30:19 +0800

    Added new post!

--- pages/Payhome.jsx
+++ pages/Payhome.jsx
@@ -1,6 +1,5 @@
-import React, {useEffect, useState} from 'react'
-import Privacypol from '../components/Privacypol'
-import Link from 'next/link';
+import React, {useState} from 'react'
+
 
 export const Amount = ({amount, setAmount, value}) => {
   return (
@@ -17,53 +16,12 @@ const initState = { values: initValues}
 function Payhome() {
   const [amount, setAmount] = useState(10)
 
-  const [state, setState] = useState(initState);
-  const { values, isLoading, error } = state;
-
-  const handleChange = ({target}) => setState((prev) => ({
-    ...prev,
-    values: {
-        ...prev.values,
-        [target.name]: target.value,
-    }
-  }))
-
   return (
     <div id="payment" className='w-full'>
       <div className='mx-auto px-2 py-20'>
             <p className='text-xl tracking-widest mt-20 uppercase text-center text-white drop-shadow-lg'>Purchasement of Application Contract </p>
-            <Privacypol />  
-            <div className='flex flex-col space-y-4 mx-auto px-4 py-6 items-center rounded-md md:w-10/12'>
-                <h2 className='textBlock-title text-[#402D29]'>Contract Form</h2>
-                <p>By filling up this form, you agree to the terms of service, and privacy policy above.</p>
-                <form>
-                  <div className='flex flex-row'>
-                    <div className='flex flex-col py-2'>
-                            <label className='uppercase text-sm py-2 '>Name <span className='text-[#ec504e]'>*</span></label>
-                            <input type='text' id='name' name='name' value={values.name} onChange={handleChange} className='border-2 rounded-lg p-3 flex border-gray-300 required:border-red-500'/>
-                    </div>
-                    <div className='flex flex-col py-2'>
-                            <label className='uppercase text-sm py-2 '>Quote No. <span className='text-[#ec504e]'>*</span></label>
-                            <input type='text' id='quote' name='quote' value={values.quote} onChange={handleChange} className='border-2 rounded-lg p-3 flex border-gray-300 required:border-red-500'/>
-                    </div>
-                  </div>
-                  
-                  <div className='flex flex-col py-2'>
-                            <label className='uppercase text-sm py-2'>Email <span className='text-[#ec504e]'>*</span></label>
-                            <input type='email' id='email' name='email' value={values.email} onChange={handleChange} className='border-2 rounded-lg p-3 flex border-gray-300 required:border-red-500'/>
-                  </div>
-                  <div className='flex flex-row'>
-                    <div className='flex flex-col py-2'>
-                            <label className='uppercase text-sm py-2 '>AdMob App ID <span className='text-[#ec504e]'>*</span></label>
-                            <input type='text' id='appid' name='appid' value={values.appid} onChange={handleChange} className='border-2 rounded-lg p-3 flex border-gray-300 required:border-red-500'/>
-                    </div>
-                    <div className='flex flex-col py-2'>
-                            <label className='uppercase text-sm py-2 '>AdMob Ad Unit ID<span className='text-[#ec504e]'>*</span></label>
-                            <input type='text' id='adunit' name='adunit' value={values.adunit} onChange={handleChange} className='border-2 rounded-lg p-3 flex border-gray-300 required:border-red-500'/>
-                    </div>
-                  </div>
-                </form>
-            </div>          
+           
+           
             <div className='flex flex-col space-y-4 mx-auto px-4 py-6 items-center rounded-md md:w-10/12'>
                 <h2 className='textBlock-title text-[#402D29]'>Payment</h2>
                 <p className='textBlock-subtitle'>Choose either full payment of $8000 or monthly instalments of $500</p>
