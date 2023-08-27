3bf5c33459affd32e3d2c75e3d75c9a7160a93d8
N-Hidaya <nurul-h@hotmail.com>
2023-08-27 20:31:02 +0800

    updated about

--- components/About.jsx
+++ components/About.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const About = () => {
@@ -7,20 +8,14 @@ const About = () => {
                 <div className="col-span-2">
                     <p className="uppercase text-xl tracking-widest text-white drop-shadow-lg">About</p>
                     <h2 className="py-4 text-[#402D29]">Who is Daya?</h2>
-                    <p className="py-2 text-gray-600">Daya is a Developer Girl who also loves to discover new things to create</p>
                     <p className="py-2 text-gray-600">
-                        Daya used to be an IT hardware engineer, serving customers in the office environment to help troubleshoot PC issues.
-                        While working full time, Daya also studies part time Degree in Computer Science. 
-                        She got headhunted and managed to work as a software engineer in a company, 
-                        however 2 years later she got laid off. It was difficult to get a job after that. 
+                    Hey there, curious minds! 🎓✨ Daya has officially tossed her graduation cap and dived into the exciting world beyond the textbooks.
+                    Post-graduation, she's taken her talents to platforms like Upwork and Fiverr, where she's weaving her freelance magic. 🪄✍️ But that's not all – right here on this blogsite, Daya's crafting her own passion-fueled projects.
                     </p>
                     <p className="py-2 text-gray-600">
-                        Fortunately, she has graduated from studies recently and started to look for other options.
-                        Daya has started working with freelance companies such as Upwork and Fiverr. 
-                        She also does her own projects that involves things that she loves to offer.
-
+                    If you're into captivating reads, head over to the <Link href='https://www.ai-daya.com/Blog' className="underline font-bold">Blog</Link> section. And tech enthusiasts, we've got your back too! Swing by the <Link href='https://www.ai-daya.com/#services' className="underline font-bold">Services</Link> section for Daya's expertise in web and mobile development.
+                    So, whether you're after inspiration or tech solutions, this is your go-to spot. Stick around for the journey ahead! 🚀🌟 #DayasAdventures"
                     </p>
-                    <p className="py-2 text-gray-600">Check out some of my latest projects.</p>
                 </div>
                 <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 bg-white rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300 ">
                     <img className="rounded-xl " src="https://i.pinimg.com/originals/44/69/bf/4469bfc00266eab9cb1e955c13cc0640.jpg" alt="programming girl" />
